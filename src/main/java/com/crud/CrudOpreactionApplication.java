0bc7f55a81b6633a25e266986575ec5fb6132633
Pradeepkomari <pradeepkumar00027@gmail.com>
2024-12-24 09:55:34 +0530

    pushed1

--- src/main/java/com/crud/CrudOpreactionApplication.java
+++ src/main/java/com/crud/CrudOpreactionApplication.java
@@ -1,13 +1,28 @@
 package com.crud;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class CrudOpreactionApplication {
+	
+	static Logger log = Logger.getLogger(CrudOpreactionApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudOpreactionApplication.class, args);
+	PropertyConfigurator.configure("log4j.properties");
+		
+		log.info("Information");
+		log.warn("Warninng");
+		log.error("Error");
+		log.debug("Debugginng");
+		log.fatal("Fatal");
+		
+		
 	}
 
 }
